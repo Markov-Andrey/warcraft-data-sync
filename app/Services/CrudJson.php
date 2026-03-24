6ad86809d5ff5a080be12a69af5843ddc4a9b1a2
Markov-Andrey <andremarkov@icloud.com>
2026-03-24 19:40:20 +0300

    add TODO

--- app/Services/CrudJson.php
+++ app/Services/CrudJson.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 class CrudJson
 {
-    // TODO есть проблема, нужно создавать поле когда его нет в json и важен тип "int" - число, и "unreal" - float, а значение храни без кавычек
+    // TODO есть проблема, нужно создавать поле когда его нет в json и важен тип "int" - число, и "unreal" - float, а значение храни без кавычек если это реально числа. вот пример - {"id":"ua1s","type":"int","level":0,"column":0,"value":21},{"id":"ua1r","type":"int","level":0,"column":0,"value":1150}. а структура json {"original":{"uplg":[{"id":"uabi","type":"string","level":0,"column":0,"value":"Aap2,A01A"}]},"custom":{"U006:Uear":[{"id":"udup","type":"int","level":0,"column":0,"value":1},{"id":"uhab","type":"string","level":0,"column":0,"value":"A018,A021,A01B,A0A8"},{"id":"ubld","type":"int",
     public static function updateValue($db, $id, $key, $value)
     {
         try {
